2057c4dd3bf1e2632a7f4fb5121b74d2e1e8a357
Craig Barratt <craigtbarratt@gmail.com>
2020-03-11 23:16:24 +0000

    initial commit of day_3 homework

---
+++ exercise_a_stops.rb
@@ -0,0 +1,73 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+
+#1. Add "Edinburgh Waverley" to the end of the array
+stops.push("Edinburgh Waverley")
+
+# OR
+# stops[6] = "Edinburgh Waverley"
+
+
+#2. Add "Glasgow Queen St" to the start of the array
+stops.unshift("Glasgow Queen St")
+
+# OR
+# stops = ["Glasgow Queen St" ] + stops
+
+
+#3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops.insert(4, "Polmont")
+
+
+#4. Print out the index position of "Linlithgow"
+p stops.index("Linlithgow")
+
+
+#5. Remove "Livingston" from the array using its name
+stops.delete("Livingston")
+
+
+#6. Delete "Cumbernauld" from the array by index
+stops.delete_at(2)
+
+
+#7. Print the number of stops there are in the array?
+p stops.length
+
+
+#8. Show as many ways as you can to return "Falkirk High" from the array?
+p stops[2]
+
+p stops.at(2)
+
+index_two = stops[2]
+p index_two
+
+rejected_indices = [0, 1, 3, 4, 5, 6]
+p stops.reject.each_with_index { |i, ix| rejected_indices.include? ix }
+
+p stops - ["Glasgow Queen St", "Croy", "Polmont", "Linlithgow", "Haymarket", "Edinburgh Waverley"]
+
+@stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+def return_falkirk()
+  p @stops[2]
+end
+return_falkirk()
+
+#OR
+# p stops.select {|s| s == "Falkirk High"}
+
+#OR
+# p stops.keep_if {|s| s == "Falkirk High"}
+
+#OR poss
+# stops = Array.new
+# stops.push "Falkirk High"
+# p stops
+
+#9. Reverse the positions of the stops in the array
+stops.reverse!
+
+#10 Print out all the stops using a for loop
+for stop in stops
+  p "#{stop}"
+end
---
+++ exercise_c_uk.rb
@@ -0,0 +1,44 @@
+united_kingdom = [
+  {
+    name: "Scotland",
+    population: 5295000,
+    capital: "Edinburgh"
+  },
+  {
+    name: "Wales",
+    population: 3063000,
+    capital: "Swansea"
+  },
+  {
+    name: "England",
+    population: 53010000,
+    capital: "London"
+  }
+]
+
+# 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+united_kingdom[1][:capital] = "Cardiff"
+
+
+# 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+united_kingdom.push({:name => "Northern Ireland", :population => 1_811_000, :capital => "Belfast"})
+
+
+# 3. Use a loop to print the names of all the countries in the UK.
+  for country_name in united_kingdom
+    country_name = country_name[:name]
+    puts country_name
+  end
+
+
+# 4. Use a loop to find the total population of the UK.
+
+def count_pop(uk_array)
+  total_pop = 0
+  for population in uk_array
+    total_pop += population[:population]
+  end
+  return total_pop
+end
+
+p total_pop = count_pop(united_kingdom)
